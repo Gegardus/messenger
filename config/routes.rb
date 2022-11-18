32637ce97600e01dca0eaee24aff694ecabae148
Gegardus <stgegard@gmail.com>
2022-11-18 22:07:35 +0400

    update styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,7 @@
 Rails.application.routes.draw do
   resource :hangouts
-
   resources :messages, only: [:create]
-
   devise_for :users  
-
-  # unauthenticated do
-  #   root 'devise/sessions#create'
-  # end
-
   root 'hangouts#index'
 
 end
